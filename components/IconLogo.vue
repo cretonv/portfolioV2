2eb651abf77ee53c811bc7b5edfb5181e9e4a882
Vincent <vincecreton@hotmail.fr>
2021-07-14 12:03:58 +0200

    🧩 Add IconLogo.vue component

---
+++ components/IconLogo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <img src="~assets/img/icon-logo.svg" alt="VC Logo" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IconLogo'
+}
+</script>
+
+<style scoped>
+  img {
+    width: 56px;
+    height: 86px;
+  }
+</style>
