48a3f474ed1f7d8714c2820add425d393f34b093
Vincent <vincecreton@hotmail.fr>
2021-08-09 13:37:57 +0200

    🚧 Site home

---
+++ pages/test.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div v-for="article in articles" :key="article.title" class="bloc-article">
+      <h1>{{ article.title }}</h1>
+      <nuxt-content :document="article" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content }) {
+    const articles = await $content('articles').fetch()
+
+    return {
+      articles
+    }
+  }
+}
+</script>
+
+<style>
+.bloc-article {
+  max-width: 800px;
+  margin: 0 auto;
+}
+</style>
