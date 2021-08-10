be592706090a8ec85a0100b28915f7641f4752f9
Vincent <vincecreton@hotmail.fr>
2021-08-10 15:48:50 +0200

    ✨Possibility to add unicons in site

---
+++ plugins/vue-unicons.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Unicon from 'vue-unicons/dist/vue-unicons-vue2.es'
+import {
+  uniArrowLeft,
+  uniArrowRight,
+  uniCheck,
+  uniSchedule,
+  uniChartGrowthAlt,
+  uniTimes,
+  uniSync
+} from 'vue-unicons/dist/icons'
+
+Unicon.add([
+  uniCheck,
+  uniArrowLeft,
+  uniSchedule,
+  uniChartGrowthAlt,
+  uniTimes,
+  uniArrowRight,
+  uniTimes,
+  uniSync
+])
+Vue.use(Unicon)
