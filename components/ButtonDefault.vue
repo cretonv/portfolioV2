7513e4c57af8bfd983fe7ae2ff9599dbe75c6c98
Vincent <vincecreton@hotmail.fr>
2021-08-12 10:12:24 +0200

    🧩 Add default button component

---
+++ components/ButtonDefault.vue
@@ -0,0 +1,45 @@
+<template>
+  <a :href="linkTo" class="btn">
+    <unicon :name="iconName" fill="#303030" height="29" width="29" />
+    <span class="text">{{ textContent }}</span>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'ButtonDefault',
+  props: {
+    iconName: {
+      type: String,
+      required: true
+    },
+    textContent: {
+      type: String,
+      required: true
+    },
+    linkTo: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.btn {
+  background: #D9D9D9;
+  color: #303030;
+  border-radius: 8px;
+  width: fit-content;
+  padding: 2px;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  .text {
+    margin: 0 12px;
+    font-family: Lato;
+    font-size: 16px;
+    font-weight: 600;
+  }
+}
+</style>
