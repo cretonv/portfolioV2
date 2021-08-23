f23df12e217732d9f48810343102628836ef92b4
Vincent <vincecreton@hotmail.fr>
2021-08-23 15:51:17 +0200

    🧩 Add TitleH2.vue component

---
+++ components/TitleH2.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="title-container">
+    <img :src="require(`~/assets/img/titleSvg/${iconName}.svg`)" />
+    <h2>{{textValue}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TitleH2',
+  props: {
+    textValue: {
+      type: String,
+      required: true
+    },
+    iconName: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .title-container {
+    position: relative;
+    max-width: fit-content;
+    margin: 24px auto;
+
+    h2 {
+      font-family: Playfair;
+      font-weight: 500;
+      font-size: 70px;
+      color: #D9D9D9;
+      text-align: center;
+      z-index: 1;
+      position: relative;
+      margin: 0;
+    }
+    img {
+      position: absolute;
+      width: 51px;
+      height: auto;
+      top: -5px;
+      left: -23px;
+      z-index: 0;
+    }
+  }
+</style>
