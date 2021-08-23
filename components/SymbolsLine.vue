18d4207742938e2f198de6b5f69370c882acd6bb
Vincent <vincecreton@hotmail.fr>
2021-08-23 18:51:26 +0200

    🧩 Add SymbolsLine.vue component and update index.vue

---
+++ components/SymbolsLine.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="line-container">
+    <div class="item">
+      <img class="symbol" src="~/assets/img/symbolsLine/air.svg">
+      <div class="label">
+        {{ textArrray[0] }}
+      </div>
+    </div>
+    <div class="item">
+      <img class="symbol" src="~/assets/img/symbolsLine/water.svg">
+      <div class="label">
+        {{ textArrray[1] }}
+      </div>
+    </div>
+    <div class="item">
+      <img class="symbol" src="~/assets/img/symbolsLine/fire.svg">
+      <div class="label">
+        {{ textArrray[2] }}
+      </div>
+    </div>
+    <div class="item">
+      <img class="symbol" src="~/assets/img/symbolsLine/earth.svg">
+      <div class="label">
+        {{ textArrray[3] }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SymbolsLine',
+  props: {
+    textArrray: {
+      type: Array,
+      required: true,
+      length: 4
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.line-container {
+  display: flex;
+  flex-flow: row nowrap;
+  max-width: 715px;
+  margin: 0 auto 32px;
+
+  .item {
+    width: 25%;
+    display: flex;
+    align-items: center;
+    flex-flow: column wrap;
+    .symbol {
+      width: 106px;
+      height: 85px;
+    }
+    .label {
+      margin-top: 8px;
+      font-family: Lato;
+      font-size: 15px;
+      font-weight: 600;
+      color: #D9D9D9;
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,24 +2,32 @@
   <div class="full-content">
     <HomeLanding />
     <TitleH2 icon-name="sun" text-value="Mon travail" />
+    <SymbolsLine :text-arrray="textArray"/>
   </div>
 </template>
 
 <script>
 import HomeLanding from '@/components/HomeLanding'
 import TitleH2 from '@/components/TitleH2'
+import SymbolsLine from '@/components/SymbolsLine'
 export default {
   head: {
     bodyAttrs: {
       class: 'no-margin'
     }
   },
-  components: { TitleH2, HomeLanding }
+  components: { TitleH2, HomeLanding, SymbolsLine },
+  data: () => {
+    return {
+      textArray: ['Conception', 'UX Design', 'Développement', 'Team Lead']
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
 .full-content {
   background: #303030;
+  padding-bottom: 100px;
 }
 </style>
